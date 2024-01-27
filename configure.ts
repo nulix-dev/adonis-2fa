ae2f99ba5b52f7573ba0f93570cc0d597f9f44a5
Bruno <bfaa1999@gmail.com>
2024-01-26 23:23:10 -0300

    feat: add configure code to add migration

--- configure.ts
+++ configure.ts
@@ -18,6 +18,28 @@ import { stubsRoot } from './stubs/main.js'
 export async function configure(command: ConfigureCommand) {
   const codemods = await command.createCodemods()
 
+  const shouldMakeMigration = await command.prompt.confirm(
+    'Do you want to create a migration to save 2FA data?',
+    {
+      default: false,
+      hint: 'You should be using @adonisjs/lucid',
+    }
+  )
+
+  if (shouldMakeMigration) {
+    const tableName = await command.prompt.ask('Enter table name you use to authentication: ', {
+      default: 'users',
+    })
+
+    await codemods.makeUsingStub(stubsRoot, 'make/migration/add_2fa_fields.stub', {
+      tableName,
+      migration: {
+        folder: 'database/migrations',
+        fileName: `${new Date().getTime()}_add_2fa_fields_to_${tableName}_table.ts`,
+      },
+    })
+  }
+
   /**
    * Publish config file
    */
